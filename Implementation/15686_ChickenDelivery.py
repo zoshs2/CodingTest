b3a731ef497ea80df845652f7d68d32751139473
Yungi <zoshs27@gmail.com>
2022-01-29 23:54:15 +0900

    Push Baekjoon Implementation 15686_ChickenDelivery.py

--- Implementation/15686_ChickenDelivery.py
+++ Implementation/15686_ChickenDelivery.py
@@ -1,14 +1,32 @@
 import sys
+import itertools # 파이썬 조합 라이브러리 (built-in 라이브러리임)
 
 if __name__=='__main__':
     N, M = map(int, input().split())
     layers = []
-    NumHouse = 0
+    HouseCoord = []
+    ChickCoord = []
     for i in range(N):
         layers.append(list(map(int, sys.stdin.readline().split())))
-        if 1 in layers[i]:
-            NumHouse += layers[i].count(1)
+        while 1 in layers[i]:
+            Hidx = layers[i].index(1)
+            HouseCoord.append([i, Hidx])
+            layers[i][Hidx] = 0
+        
+        while 2 in layers[i]:
+            Cidx = layers[i].index(2)
+            ChickCoord.append([i, Cidx])
+            layers[i][Cidx] = 0
 
-    print(NumHouse)
+    ChickNum = len(ChickCoord)
+    ChickDist = []
+    for r1, c1 in HouseCoord:
+        DistTemp = []
+        for r2, c2 in ChickCoord:
+            DistTemp.append(abs(r1-r2)+abs(c1-c2))
+        ChickDist.append(DistTemp)
     
+    MinDist = 0
+    NumOfCase = list(itertools.combinations(range(ChickNum), M))
+
     
