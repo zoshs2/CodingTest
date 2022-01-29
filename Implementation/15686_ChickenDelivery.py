5bf38b30e34d032f93282ad2501c07cef67b42cd
Yungi <zoshs27@gmail.com>
2022-01-30 00:20:13 +0900

    Push Baekjoon Implementation 15686_ChickenDelivery.py

--- Implementation/15686_ChickenDelivery.py
+++ Implementation/15686_ChickenDelivery.py
@@ -1,24 +1,24 @@
 import sys
 import itertools # 파이썬 조합 라이브러리 (built-in 라이브러리임)
+import math # math.inf 쓰려고 
 
 if __name__=='__main__':
     N, M = map(int, input().split())
-    layers = []
     HouseCoord = []
     ChickCoord = []
     for i in range(N):
-        layers.append(list(map(int, sys.stdin.readline().split())))
-        while 1 in layers[i]:
-            Hidx = layers[i].index(1)
+        layer = list(map(int, sys.stdin.readline().split()))
+        while 1 in layer:
+            Hidx = layer.index(1)
             HouseCoord.append([i, Hidx])
-            layers[i][Hidx] = 0
+            layer[Hidx] = 0
         
-        while 2 in layers[i]:
-            Cidx = layers[i].index(2)
+        while 2 in layer:
+            Cidx = layer.index(2)
             ChickCoord.append([i, Cidx])
-            layers[i][Cidx] = 0
+            layer[Cidx] = 0
 
-    ChickNum = len(ChickCoord)
+    ChickNum = len(ChickCoord) # 지도에 있는 전체 치킨집 갯수
     ChickDist = []
     for r1, c1 in HouseCoord:
         DistTemp = []
@@ -26,7 +26,14 @@ if __name__=='__main__':
             DistTemp.append(abs(r1-r2)+abs(c1-c2))
         ChickDist.append(DistTemp)
     
-    MinDist = 0
+    MinDist = math.inf
     NumOfCase = list(itertools.combinations(range(ChickNum), M))
+    for case in NumOfCase:
+        CityDist = 0
+        for dist in ChickDist:
+            CityDist += min([dist[x] for x in case])
+        
+        if CityDist < MinDist:
+            MinDist = CityDist
 
-    
+    print(MinDist)
