8240e84a60022b69c43e66f8712ada732b9a0d64
Yungi <zoshs27@gmail.com>
2022-02-03 23:46:58 +0900

    Push Baekjoon Greedy 1080_Matrix.py

--- Greedy/1080_Matrix.py
+++ Greedy/1080_Matrix.py
@@ -12,16 +12,29 @@ for i in range(2):
         arr = list(map(int, list(sys.stdin.readline().strip())))
         before.append(list(map(bool, arr)))  
 
+BoolMatrix = []
+for i in range(N):
+    BoolMatrix.append([True if ele[0]==ele[1] else False for ele in zip(before[i], after[i])])
+
 q1, r1 = divmod(M, 3) # 3 by 3 행렬을 좌우로 몇번 서칭할 수 있는지 = 몫과 나머지를 더한 값
 new_m = q1 + r1
-q2, r2 = divmod(N, 3)
+q2, r2 = divmod(N, 3) # 상하로 몇번 서칭할 수 있는지
 new_n = q2 + r2
 if q1 == 0 or q2 == 0: # 만약 N이나 M이 3미만이라서 한번도 서칭할 수 없다면, -1 반환
     sys.exit("-1")
 
+cnt = 0
+print(BoolMatrix)
 for i in range(new_n):
     for j in range(new_m):
-        arr1 = before[i][j:j+3]
-        arr2 = after[i][j:j+3]
-        
-    
+       if BoolMatrix[i][j] == False:
+           BoolMatrix[i][j:j+3] = [not ele for ele in BoolMatrix[i][j:j+3]]
+           BoolMatrix[i+1][j:j+3] = [not ele for ele in BoolMatrix[i+1][j:j+3]]
+           BoolMatrix[i+2][j:j+3] = [not ele for ele in BoolMatrix[i+2][j:j+3]]
+           cnt += 1
+
+CheckFC = [ele for row in BoolMatrix for ele in row]
+if sum(CheckFC) != N*M:
+    sys.exit("-1")
+else:    
+    print(cnt)
