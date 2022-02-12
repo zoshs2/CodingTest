5266c9c711ce8d37313ff8604fda63b88a8d71f3
Yungi <zoshs27@gmail.com>
2022-02-12 19:00:57 +0900

    Push Baekjoon Greedy 1461_Library.py

--- Greedy/1461_Library.py
+++ Greedy/1461_Library.py
@@ -1,8 +1,23 @@
 import sys
 
+def HowStep(sign_arr):
+    steps = 0
+    
+    return steps
+
+
 N, M = map(int, sys.stdin.readline().strip().split())
-arr = sorted(list(map(int, sys.stdin.readline().strip().split())))
+arr = list(map(int, sys.stdin.readline().strip().split()))
+neg_arr = []
+pos_arr = []
+for loc in arr:
+    if loc < 0:
+        neg_arr.append(loc)
+    else:
+        pos_arr.append(loc)
+
 if N <= M:
+    
     print(max(arr))
     exit(0)
 
